529469a9fca0da419edd3da689bcd837798fb2fc
Summer <ssdf90771@gmail.com>
2025-11-08 20:39:39 +0800

    fix: update config for cms to resolve 500 error when login

--- apps/cms/config/server.ts
+++ apps/cms/config/server.ts
@@ -1,6 +1,10 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('PUBLIC_URL', 'https://fullstack-lesson-1.zeabur.app'),
+  proxy: {
+    koa: true
+  },
   app: {
     keys: env.array('APP_KEYS'),
   },
